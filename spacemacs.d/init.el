b287c3c5c43430cfba56549265fed24bd4e09989
Abhinav Gupta <mail@abhinavg.net>
2016-10-28 17:35:15 -0700

    spacemacs: ignore PATH being set in zshrc

--- spacemacs.d/init.el
+++ spacemacs.d/init.el
@@ -292,6 +292,8 @@ executes.
  This function is mostly useful for variables that need to be set
 before packages are loaded. If you are unsure, you should try in setting them in
 `dotspacemacs/user-config' first."
+
+  (setq exec-path-from-shell-check-startup-files nil)
   )
 
 (defun dotspacemacs/user-config ()
