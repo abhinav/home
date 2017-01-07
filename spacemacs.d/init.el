d560199a6df44aefcedb0c3ec96a250a1b37ca1f
Abhinav Gupta <mail@abhinavg.net>
2017-01-06 21:14:15 -0800

    spacemacs: Use standard font and size

--- spacemacs.d/init.el
+++ spacemacs.d/init.el
@@ -129,8 +129,8 @@ values."
    dotspacemacs-colorize-cursor-according-to-state t
    ;; Default font, or prioritized list of fonts. `powerline-scale' allows to
    ;; quickly tweak the mode-line size to make separators look not too crappy.
-   dotspacemacs-default-font '("Source Code Pro"
-                               :size 13
+   dotspacemacs-default-font '("Liberation Mono for Powerline"
+                               :size 12
                                :weight normal
                                :width normal
                                :powerline-scale 1.1)
