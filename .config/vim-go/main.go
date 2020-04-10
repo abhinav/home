737a07c56486029871c74b94bceb1b98c5c29297
Abhinav Gupta <abg@uber.com>
2020-04-10 09:05:26 -0700

    vim-go: Include flag parsing up in main template

--- .config/vim-go/main.go
+++ .config/vim-go/main.go
@@ -1,18 +1,27 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
+	"os"
 )
 
 func main() {
 	log.SetFlags(0)
-	if err := run(); err != nil {
+	if err := run(os.Args[1:]); err != nil {
 		log.Fatalf("%+v", err)
 	}
 }
 
-func run() error {
+func run(args []string) error {
+	flag := flag.NewFlagSet(os.Args[0], flag.ContinueOnError)
+	// ...
+
+	if err := flag.Parse(args); err != nil {
+		return err
+	}
+
 	fmt.Println("hello")
 	return nil
 }
