1cc7bf9b04d6b22ce420ad1e1134cbb51d6384ae
Abhinav Gupta <abhinav@pulumi.com>
2023-02-01 08:38:12 -0800

    nvim: Add treesitter-context

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -90,6 +90,15 @@ require('lazy').setup({
 			})
 		end,
 	},
+	{
+		'nvim-treesitter/nvim-treesitter-context',
+		dependencies = {'nvim-treesitter/nvim-treesitter'},
+		config = function()
+			require('treesitter-context').setup {
+				enable = true,
+			}
+		end,
+	},
 	{'tpope/vim-abolish', command = "S"},
 	'tpope/vim-repeat',
 	'tpope/vim-sleuth',
