d597354be7d93512212f4281e7738189060dc8f9
Abhinav Gupta <abhinav@pulumi.com>
2023-06-14 13:11:04 -0700

    nvim/gitsigns: drop null_ls code action
    This is noisier than desirable.

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -433,7 +433,6 @@ require('lazy').setup({
 			local null_ls = require('null-ls')
 			null_ls.setup({
 				sources = {
-					null_ls.builtins.code_actions.gitsigns,
 					null_ls.builtins.code_actions.shellcheck,
 					null_ls.builtins.diagnostics.actionlint,
 					null_ls.builtins.diagnostics.shellcheck,
