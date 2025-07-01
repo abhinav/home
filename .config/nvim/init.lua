7b6e018679d22d1a942f6414721cdc824a739803
Abhinav Gupta <mail@abhinavg.net>
2025-07-01 10:20:29 -0700

    nvim: Drop neogit
    Unused

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -433,26 +433,6 @@ require('lazy').setup({
 			enhanced_diff_hl = true,
 		},
 	},
-	{
-		"NeogitOrg/neogit",
-		branch = 'master',
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"sindrets/diffview.nvim",
-			"nvim-telescope/telescope.nvim",
-		},
-		config = function()
-			local neogit = require('neogit')
-			neogit.setup {
-				remember_settings = false,
-				graph_style = 'unicode',
-			}
-
-			vim.keymap.set('n', '<leader>gg', function()
-				neogit.open({ cwd = vim.fn.expand('%:p:h') })
-			end, {desc = "Open neogit"})
-		end,
-	},
 
 
 	-- Look and feel {{{2
